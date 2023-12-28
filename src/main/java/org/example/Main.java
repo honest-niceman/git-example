5c7b2c159009883032a61f686b13d812d55b978d
Georgii Vlasov <honest.niceman@gmail.com>
2023-12-28 20:13:11 +0300

    Delete todo messages

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //todo add more code
+        //todo add more code // can't do it until 2024
         //fixme
     }
 }
